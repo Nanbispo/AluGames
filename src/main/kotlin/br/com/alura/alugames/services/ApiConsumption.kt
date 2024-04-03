2eb1da1604980414772b4256113026767d79a438
Renan Bispo <contato.renanjesusbispo@gmail.com>
2024-04-03 15:23:43 -0300

    refatoração do código, encapsulando a busca de dados para usá-lo em diferentes métodos que façam requisição http

--- src/main/kotlin/br/com/alura/alugames/services/ApiConsumption.kt
+++ src/main/kotlin/br/com/alura/alugames/services/ApiConsumption.kt
@@ -6,36 +6,32 @@ import br.com.alura.alugames.utilitys.createGamer
 import com.google.gson.Gson
 import com.google.gson.reflect.TypeToken
 import org.example.br.com.alura.alugames.models.InfoGames
-import java.lang.ProcessHandle.Info
 import java.net.URI
 import java.net.http.HttpClient
 import java.net.http.HttpRequest
 import java.net.http.HttpResponse
 
 class ApiConsumption {
-    fun searchGame(id: String): InfoGames{
-        val addres = "https://www.cheapshark.com/api/1.0/games?id=$id"
+
+    private fun consumeData(addres: String): String{
         val client: HttpClient = HttpClient.newHttpClient()
         val request = HttpRequest.newBuilder()
             .uri(URI.create(addres))
             .build()
         val response = client
             .send(request, HttpResponse.BodyHandlers.ofString())
-        val json = response.body()
+        return response.body()
+    }
+    fun searchGame(id: String): InfoGames{
+        val addres = "https://www.cheapshark.com/api/1.0/games?id=$id"
+        val json = consumeData(addres)
         val gson = Gson()
         val myInfoGaming = gson.fromJson(json, InfoGames::class.java)
         return myInfoGaming
     }
-    fun searchGamerJson(): List<Gamer>{
+    fun searchGamer(): List<Gamer>{
         val addres = "https://raw.githubusercontent.com/jeniblodev/arquivosJson/main/gamers.json"
-        val client: HttpClient = HttpClient.newHttpClient()
-        val request = HttpRequest.newBuilder()
-            .uri(URI.create(addres))
-            .build()
-        val response = client
-            .send(request, HttpResponse.BodyHandlers.ofString())
-        val json = response.body()
-
+        val json = consumeData(addres)
         val gson = Gson()
         val convertTypeGamer = object: TypeToken<List<InfoGamerJson>>(){}.type
         val gamersList: List<InfoGamerJson> = gson.fromJson(json, convertTypeGamer)
