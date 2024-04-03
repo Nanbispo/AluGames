aabdc37c68bf98dee5e44a4c00333317da5efc87
Renan Bispo <contato.renanjesusbispo@gmail.com>
2024-04-03 15:23:52 -0300

    testes e conclusão do primeiro modulo

--- src/main/kotlin/br/com/alura/alugames/main/TestingJson.kt
+++ src/main/kotlin/br/com/alura/alugames/main/TestingJson.kt
@@ -4,6 +4,9 @@ import br.com.alura.alugames.services.ApiConsumption
 
 fun main (){
     val consume = ApiConsumption()
-    val gamersList = consume.searchGamerJson()
+    val gamersList = consume.searchGamer()
     println(gamersList)
+
+    val gamesList = consume.searchGame("151")
+    println(gamesList)
 }
