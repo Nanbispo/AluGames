d89535be934349222fd2a4b87f4db89291f22f67
Renan Bispo <contato.renanjesusbispo@gmail.com>
2024-04-04 15:33:09 -0300

    filtro de recomendação no plano prata

--- src/main/kotlin/br/com/alura/alugames/models/subscriptionPlan.kt
+++ src/main/kotlin/br/com/alura/alugames/models/subscriptionPlan.kt
@@ -3,16 +3,20 @@ package br.com.alura.alugames.models
 class subscriptionPlan(
     type: String,
     val monthlyPayment: Double,
-    val gamesIncluded: Int
-) : Plan(type) {
+    val gamesIncluded: Int,
+    val percentageDiscountReputation: Double) : Plan(type) {
 
     override fun getValue(rent: Rent): Double {
         val totalGamesInMonth = rent.gamer.gamesOfTheMonth(rent.period.initialDate.monthValue).size + 1
 
-        return if (totalGamesInMonth <= 3) {
+        return if (totalGamesInMonth <= gamesIncluded) {
             0.0
         } else {
-            super.getValue(rent)
+            var originalValue = super.getValue(rent)
+            if (rent.gamer.average > 8){
+                originalValue = originalValue * percentageDiscountReputation
+            }
+            originalValue
         }
     }
 }
