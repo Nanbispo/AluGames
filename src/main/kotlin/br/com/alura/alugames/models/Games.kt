5200ca5d92befdd76949bdd79e0c7e8b434109e3
Renan Bispo <contato.renanjesusbispo@gmail.com>
2024-04-04 15:40:16 -0300

    desafio:

--- src/main/kotlin/br/com/alura/alugames/models/Games.kt
+++ src/main/kotlin/br/com/alura/alugames/models/Games.kt
@@ -1,13 +1,25 @@
 package org.example.br.com.alura.alugames.models
 
+import br.com.alura.alugames.repositories.Recommendable
 import jdk.jfr.Description
 
 data class Games(
     var title: String,
     var thumb: String
-) {
+) : Recommendable {
     var description: String? = null
     var preco = 0.0
+    private val listNotes = mutableListOf<Int>()
+
+    override val average: Double
+        get() = listNotes.average()
+
+    override fun toRecommend(note: Int) {
+        if (note < 1 || note > 10) {
+            println("Nota invalida")
+        }
+        listNotes.add(note)
+    }
 
     constructor(
         title: String,
