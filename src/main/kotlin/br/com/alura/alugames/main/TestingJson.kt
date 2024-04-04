f67b9653c5fddd68d657ec4e46dbea2b43f70cc4
Renan Bispo <contato.renanjesusbispo@gmail.com>
2024-04-04 10:18:56 -0300

    conclusão do segundo modulo e testes

--- src/main/kotlin/br/com/alura/alugames/main/TestingJson.kt
+++ src/main/kotlin/br/com/alura/alugames/main/TestingJson.kt
@@ -1,15 +1,34 @@
 package br.com.alura.alugames.main
 
+import br.com.alura.alugames.models.Period
 import br.com.alura.alugames.services.ApiConsumption
+import java.time.LocalDate
 
 fun main (){
     val consume = ApiConsumption()
     val gamersList = consume.searchGamer()
-    println(gamersList)
-
-    val gamesList = consume.searchGame("151")
-    println(gamesList)
-
     val gamesJson = consume.searchGameJson()
-    println(gamesJson)
+
+    //println(gamersList)
+    //println(gamesList)
+    //println(gamesJson)
+
+    val jogador = gamersList.get(3)
+    val jogo = gamesJson.get(10)
+    val jogo2 = gamesJson.get(3)
+    val jogo3 = gamesJson.get(13)
+
+
+    val periodo1 = Period(LocalDate.now(), LocalDate.now().plusDays(7))
+    val periodo2 = Period(LocalDate.now(), LocalDate.now().plusDays(3))
+    val periodo3 = Period(LocalDate.now(), LocalDate.now().plusDays(10))
+
+    jogador.rentGame(jogo, periodo1)
+    jogador.rentGame(jogo2,periodo2)
+    jogador.rentGame(jogo3,periodo3)
+
+
+    println(jogador.rentendGames)
+
+
 }
